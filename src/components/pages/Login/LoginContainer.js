ed5e6d958a6fd28069a935813aac7d7fc7002c44
glebone23 <gleblishishyn@gmail.com>
2018-10-28 23:36:42 +0200

    changeEmail and changePassword handlers in LoginContainer actually check isLoading before setState()

--- src/components/pages/Login/LoginContainer.js
+++ src/components/pages/Login/LoginContainer.js
@@ -6,6 +6,7 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { loginUser } from 'actions';
 import { getUserInfo } from 'selectors';
+// import validateEmail from 'helpers';
 import Login from './Login';
 
 export const enhance = compose(
@@ -25,19 +26,23 @@ export const enhance = compose(
   withState('password', 'setPassword', ''),
   withState('isLoading', 'setLoading', false),
   withHandlers({
-    changeEmail: ({ setEmail }) => event => setEmail(event.currentTarget.value),
-    changePassword: ({ setPassword }) => event => setPassword(event.currentTarget.value),
+    changeEmail: ({ setEmail, isLoading }) => event => (!isLoading ? setEmail(event.currentTarget.value) : null),
+    changePassword: ({ setPassword, isLoading }) => event => (
+      !isLoading ? setPassword(event.currentTarget.value) : null
+    ),
     handleLogin: ({
       email, password, setLoading, isLoading, dispatchLoginUser, history,
     }) => (event) => {
       event.preventDefault();
+      // TODO validate email and password!
+      // TODO add alert(div) with errors while logging in before login form!
       /* eslint-disable no-console */
       if (!isLoading) {
         setLoading(true);
         dispatchLoginUser({ email, password })
           .then(() => history.push('/'))
           .catch(() => {
-            console.log('Wrong email or password');
+            console.log('Incorrect email or password');
             setLoading(false);
           });
       }
