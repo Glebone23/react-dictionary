e2778003d2cff38723c3306c841f6e81809c4195
Glebone23 <gleblishishyn@gmail.com>
2018-10-30 18:27:44 +0200

    added withError HOC which currently handle server error

---
+++ src/components/HOCs/withError.js
@@ -0,0 +1,13 @@
+import { compose, setDisplayName, withHandlers } from 'recompose';
+
+const withError = compose(
+  setDisplayName('CheckAuthentication'),
+  withHandlers({
+    handleError: () => ({ status }) => {
+      if (status === 401) return { logout: true };
+      return null;
+    },
+  }),
+);
+
+export default withError;
